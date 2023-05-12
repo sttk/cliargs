5dd0da34cfd9d210b3449ab67eb0444b6c31f376
sttk <sttk.xslet@gmail.com>
2023-05-12 20:21:42 +0900

    new: added FindFirstArg function

---
+++ find_test.go
@@ -0,0 +1,61 @@
+package cliargs_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/sttk-go/cliargs"
+	"testing"
+)
+
+func TestFindFirstArg_findAtFirstIndex(t *testing.T) {
+	osArgs := []string{"app", "foo-bar", "-a", "--baz", "--bcd"}
+
+	i, arg, exists := cliargs.FindFirstArg(osArgs)
+	assert.Equal(t, i, 1)
+	assert.Equal(t, arg, "foo-bar")
+	assert.True(t, exists)
+}
+
+func TestFindFirstArg_findAtMiddleIndex(t *testing.T) {
+	osArgs := []string{"app", "--corge", "foo-bar", "-a", "--baz", "--bcd"}
+
+	i, arg, exists := cliargs.FindFirstArg(osArgs)
+	assert.Equal(t, i, 2)
+	assert.Equal(t, arg, "foo-bar")
+	assert.True(t, exists)
+}
+
+func TestFindFirstArg_findAtLastIndex(t *testing.T) {
+	osArgs := []string{"app", "--corge", "--foo-bar", "-a", "baz"}
+
+	i, arg, exists := cliargs.FindFirstArg(osArgs)
+	assert.Equal(t, i, 4)
+	assert.Equal(t, arg, "baz")
+	assert.True(t, exists)
+}
+
+func TestFindFirstArg_returnFoundFirst(t *testing.T) {
+	osArgs := []string{"app", "--corge", "foo-bar", "-a", "baz", "--bcd"}
+
+	i, arg, exists := cliargs.FindFirstArg(osArgs)
+	assert.Equal(t, i, 2)
+	assert.Equal(t, arg, "foo-bar")
+	assert.True(t, exists)
+}
+
+func TestFindFirstArg_notFound(t *testing.T) {
+	osArgs := []string{"app", "--corge", "--foo-bar", "-a", "-baz", "--bcd"}
+
+	i, arg, exists := cliargs.FindFirstArg(osArgs)
+	assert.Equal(t, i, -1)
+	assert.Equal(t, arg, "")
+	assert.False(t, exists)
+}
+
+func TestFindFirstArg_supportDoubleHyphens(t *testing.T) {
+	osArgs := []string{"app", "--", "--foo-bar", "-a", "-baz", "--bcd"}
+
+	i, arg, exists := cliargs.FindFirstArg(osArgs)
+	assert.Equal(t, i, 2)
+	assert.Equal(t, arg, "--foo-bar")
+	assert.True(t, exists)
+}
